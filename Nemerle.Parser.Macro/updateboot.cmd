bfc498019db3f34e3b0c6427cc37bdcd83c9c221
rampelstinskin <rampelstinskin@gmail.com>
2012-04-03 21:36:38 +0600

    Work on bootstrap. Refactoring.

--- Nemerle.Parser.Macro/updateboot.cmd
+++
@@ -1,2 +0,0 @@
-xcopy bin\Debug\Nemerle.Parser.Macro.dll boot\Nemerle.Parser.Macro.dll /Y
-xcopy ..\Nemerle.Parser\bin\Debug\Nemerle.Parser.dll boot\Nemerle.Parser.dll /Y
---
+++ updateboot.cmd
@@ -0,0 +1,2 @@
+xcopy Nemerle.Parser.Macro\bin\Debug\Nemerle.Parser.Macro.dll boot\Nemerle.Parser.Macro.dll /Y
+xcopy Nemerle.Parser\bin\Debug\Nemerle.Parser.dll boot\Nemerle.Parser.dll /Y
