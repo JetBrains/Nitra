04c47300ff7ffb242a94c8bca89f3ed697eebdc4
VladD2 <vc@rsdn.ru>
2012-05-20 02:17:42 +0400

    Finish bootstrapping. At present the Nemerle.Parser use .n2 format. Also improved updateboot.cmd.

--- updateboot.cmd
+++ updateboot.cmd
@@ -1,2 +1,15 @@
-xcopy Nemerle.Parser.Macro\bin\Debug\Nemerle.Parser.Macro.dll boot\Nemerle.Parser.Macro.dll /Y
-xcopy Nemerle.Parser\bin\Debug\Nemerle.Parser.dll boot\Nemerle.Parser.dll /Y
+echo off
+set Nemerle="%ProgramFiles%\Nemerle\Net-4.0"
+set Boot=boot\Net-4.0
+set CompilerFiles=ncc.exe ncc.pdb ncc32.exe ncc32.pdb ncc64.exe ncc64.pdb Nemerle.dll Nemerle.pdb Nemerle.Compiler.dll Nemerle.Compiler.pdb Nemerle.Macros.dll Nemerle.Macros.pdb Nemerle.MSBuild.targets Nemerle.MSBuild.Tasks.dll
+
+ROBOCOPY Nemerle.Parser.Macro\bin\Debug\        %Boot%\   Nemerle.Parser.Macro.???           /LOG:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY Nemerle.Parser\bin\Debug\              %Boot%\   Nemerle.Parser.???                /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY Nemerle.Parser.Macro.Parser\bin\Debug\ %Boot%\   Nemerle.Parser.Macro.Parser.???   /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY Nemerle.Parser.Macro.Model\bin\Debug\  %Boot%\   Nemerle.Parser.Macro.Model.???    /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY %Nemerle%\                             %Boot%\   %CompilerFiles%                   /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+
+cls
+echo Result of update %Boot%
+type %Boot%\Boot-update.log
+pause
