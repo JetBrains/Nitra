a117b282062005bb340f9d021eb7948e2a41eebb
VladD2 <vc@rsdn.ru>
2012-08-09 15:11:59 +0400

    Project renaming.

--- N2/updateboot.cmd
+++ N2/updateboot.cmd
@@ -3,12 +3,12 @@ set Nemerle="%ProgramFiles%\Nemerle\Net-4.0"
 set Boot=%dp0\boot\Net-4.0
 set CompilerFiles=ncc.exe ncc.pdb ncc32.exe ncc32.pdb ncc64.exe ncc64.pdb Nemerle.dll Nemerle.pdb Nemerle.Compiler.dll Nemerle.Compiler.pdb Nemerle.Macros.dll Nemerle.Macros.pdb Nemerle.MSBuild.targets Nemerle.MSBuild.Tasks.dll
 
-ROBOCOPY Nemerle.Parser.Macro\bin\Debug\        %Boot%\   Nemerle.Parser.Macro.???           /LOG:%Boot%\Boot-update.log /NP /NJS /V /NJH
-ROBOCOPY Nemerle.Parser\bin\Debug\              %Boot%\   Nemerle.Parser.???                /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
-ROBOCOPY Nemerle.Parser.Macro.Parser\bin\Debug\ %Boot%\   Nemerle.Parser.Macro.Parser.???   /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
-ROBOCOPY Nemerle.Parser.Macro.Model\bin\Debug\  %Boot%\   Nemerle.Parser.Macro.Model.???    /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
-ROBOCOPY %Nemerle%\                             %Boot%\   %CompilerFiles%                   /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
-ROBOCOPY Nemerle.Parser\                        Boot.Nemerle.Parser\   *.n *.nproj          /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH /MIR /A+:R /XF Boot.Nemerle.Parser.nproj 
+ROBOCOPY  N2.Compiler\bin\Debug\   %Boot%\           N2.Compiler.???   /LOG:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY  N2.Runtime\bin\Debug\    %Boot%\           N2.Runtime.???    /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY  N2.Grammar\bin\Debug\    %Boot%\           N2.Grammar.???    /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY  N2.Model\bin\Debug\      %Boot%\           N2.Model.???      /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY  %Nemerle%\               %Boot%\           %CompilerFiles%   /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
+ROBOCOPY  N2.Runtime\              Boot.N2.Runtime\  *.n *.nproj       /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH /MIR /A+:R /XF Boot.N2.Runtime.nproj 
 cls
 echo Result of update %Boot%
 type %Boot%\Boot-update.log
