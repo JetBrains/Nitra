4da886ab651b002252acab8211ede9887773e428
VladD2 <vc@rsdn.ru>
2012-08-31 22:16:01 +0400

    Work on C# parser.

---
+++ CSharp.Grammar.Test/Tests/Test-001.cs
@@ -0,0 +1,15 @@
+﻿using Nemerle;
+using Nemerle.Collections;
+using Nemerle.Text;
+using Nemerle.Utility;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Grammar.Test
+{
+  class Test_001
+  {
+  }
+}
