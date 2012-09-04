82f6d22ec0e4708989c0466b2f46f98f3fcc38e0
VladD2 <vc@rsdn.ru>
2012-09-04 23:13:02 +0400

    Work on C# parser. Added PropertyDeclaration, FieldDeclaration and ConstantDeclaration.

--- CSharp.Grammar.Test/Tests/Test-001.cs
+++ CSharp.Grammar.Test/Tests/Test-001.cs
@@ -12,9 +12,17 @@ namespace CSharp.Grammar.Test
   using System.Linq;
 
   [A(aaa)]
-  class Test_001 : A<B>.Ccc<int>.X<Y<Z.A<@int>>>, ITest<int>
+  class Test_001<T, X> : A<T>.Ccc<int>.X<Y<Z.A<@int>>>, ITest<int>, string, int
+    where T : class, new()
+    where X : ITest<X>
   {
     public static int Foo(this string x, ref int r) { ;;; }
+
+    string Prop1 { get; set; }
+
+    int[] _field = {1, 2};
+
+    const A x = y;
   }
 }
 
@@ -22,4 +30,9 @@ enum E : byte
 {
   // A, // FIXME: Bug in implementation of cycle
   A, B
+}
+
+class A
+{
+  public readonly int Field1 = 42;
 }
