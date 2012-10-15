bd230a33502bb60c04973e078f9e46aacca82973
VladD2 <vc@rsdn.ru>
2012-10-15 22:26:57 +0400

    Work on transition of N2 project to attributes.  Update boot. Move the RegularRule to attributes. Add Equals/GetHashCode/ToString to RegularRule.

--- N2/updateboot.cmd
+++ N2/updateboot.cmd
@@ -6,7 +6,6 @@ set CompilerFiles=ncc.exe ncc.pdb ncc32.exe ncc32.pdb ncc64.exe ncc64.pdb Nemerl
 ROBOCOPY  N2.Compiler\bin\Debug\   %Boot%\           N2.Compiler.???   /LOG:%Boot%\Boot-update.log /NP /NJS /V /NJH
 ROBOCOPY  N2.Runtime\bin\Debug\    %Boot%\           N2.Runtime.???    /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
 ROBOCOPY  N2.Grammar\bin\Debug\    %Boot%\           N2.Grammar.???    /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
-ROBOCOPY  N2.Model\bin\Debug\      %Boot%\           N2.Model.???      /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
 ROBOCOPY  %Nemerle%\               %Boot%\           %CompilerFiles%   /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH
 ROBOCOPY  N2.Runtime\              Boot.N2.Runtime\  *.n *.nproj       /LOG+:%Boot%\Boot-update.log /NP /NJS /V /NJH /MIR /A+:R /XF Boot.N2.Runtime.nproj 
 cls
