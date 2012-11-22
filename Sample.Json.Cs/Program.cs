03083b278516881312e2f5c53543bd107b98b8dd
VladD2 <vc@rsdn.ru>
2012-11-22 15:07:22 +0400

    Remove tuples from public interface. Add C# sample project for Join,Grammar.

---
+++ Sample.Json.Cs/Program.cs
@@ -0,0 +1,55 @@
+﻿using N2;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample.Json.Cs
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var source = new SourceSnapshot(text);
+      var parserHost = new ParserHost();
+      var parseResult = parserHost.DoParsing(source, JsonParser.GrammarImpl.StartRuleDescriptor);
+      if (parseResult.IsSuccess)
+      {
+        var ast = parseResult.CreateAst<JsonParser.Start.Ast>();
+        Console.WriteLine("Pretty print: " + ast);
+        Console.WriteLine();
+      }
+      else
+      {
+        var errors = parseResult.CollectErrors();
+        var pos    = source.PositionToLineColumn(errors.Position);
+        Console.WriteLine("Parse error at (${0}, ${1}), rules: {2})", pos.Line, pos.Column, string.Join(", ", errors.Messges));
+      }
+    }
+
+    static string text = 
+  @"{
+		  'glossary': {
+				  'title': 'example glossary',
+		  'GlossDiv': {
+						  'title': 'S',
+			  'GlossList': {
+								  'GlossEntry': {
+										  'ID': 'SGML',
+					  'SortAs': 'SGML',
+					  'GlossTerm': 'Standard Generalized Markup Language',
+					  'Acronym': 'SGML',
+					  'Abbrev': 'ISO 8879:1986',
+					  'GlossDef': {
+												  'para': 'A meta-markup language, used to create markup languages such as DocBook.',
+						  'GlossSeeAlso': ['GML', 'XML']
+										  },
+					  'GlossSee': 'markup'
+								  }
+						  }
+				  }
+		  }
+  ";
+  }
+}
