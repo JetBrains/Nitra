1372286c17fb34f2ffbc8233139fa8e7e8ee6068
VladD2 <vc@rsdn.ru>
2012-12-04 23:38:59 +0400

    Merge remote-tracking branch 'origin/hardcase_work' into VladD2_work
    Conflicts:
    	N2/N2.Compiler/Util.n

--- Tests/run2.cmd
+++ Tests/run2.cmd
@@ -1,16 +1,16 @@
-rem @echo off
+@echo off
 set Configuration=Debug
 set Nemerle="%ProgramFiles%\Nemerle\Net-4.0"
-set OutDir=Bin\%Configuration%\Positive
-set RuntimeDllPath=..\N2\N2.Runtime\bin\%Configuration%
-
+set OutDir=%~dp0\Bin\%Configuration%\Positive
+set RuntimeDllPath=%~dp0\..\N2\N2.Runtime\bin\%Configuration%
+set Positive=%~dp0\!Positive
 rmdir %OutDir% /S /Q
 mkdir %OutDir%
 rem copy %Nemerle%\Nemerle.* %OutDir%\Nemerle.* /B /Z
 rem copy %RuntimeDllPath%\N2.Runtime.* %OutDir%  /B /Z
-pause
-copy Bin\Grammars\*.* %OutDir% /B /Z
-pause
-
-%Nemerle%\Nemerle.Compiler.Test.exe Positive\*.n Positive\*.cs Positive\*.n2 -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\N2.Runtime.dll
+copy %~dp0\..\Grammars\Bin\%Configuration%\*.* %OutDir% /B /Z 1>0
+pushd .
+cd %OutDir%
+%Nemerle%\Nemerle.Compiler.Test.exe %Positive%\*.n %Positive%\*.cs %Positive%\*.n2 -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\N2.Runtime.dll
+popd
 pause
