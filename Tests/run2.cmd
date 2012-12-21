e9f622a68ac70d0132a1cdd3cb67eef46f3e3eba
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2012-12-21 23:22:14 +0400

    Fix run2.cmd

--- Tests/run2.cmd
+++ Tests/run2.cmd
@@ -1,6 +1,6 @@
 @echo off
 if not defined Configuration set Configuration=Debug
-if not defined NemerleBinPathRoot set NemerleBinPathRoot=%ProgramFiles%\Nemerle
+if not defined NemerleBinPathRoot set NemerleBinPathRoot="%ProgramFiles%\Nemerle"
 if not defined Nemerle set Nemerle=%NemerleBinPathRoot%\Net-4.0
 set RuntimeDllPath=%~dp0\..\N2\N2.Runtime\bin\%Configuration%
 set N2CompilerDllPath=%~dp0\..\N2\N2.Compiler\bin\%Configuration%\Stage2
