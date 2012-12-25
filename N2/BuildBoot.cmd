91188229f5ddcd6f3cbd7ff6c86a3314cad903aa
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2012-12-25 18:06:33 +0400

    Fix BootTasks.proj

--- N2/BuildBoot.cmd
+++ N2/BuildBoot.cmd
@@ -1,3 +1,3 @@
 @echo off
-%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\boottasks.proj /t:BuildBoot /tv:4.0
+%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\BootTasks.proj /t:BuildBoot /tv:4.0
 pause
