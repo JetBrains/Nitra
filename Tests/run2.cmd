b737803ecb995203cd7af9da4d64cd1a24fcd1cf
VladD2 <vc@rsdn.ru>
2013-02-08 18:55:48 +0400

    Remove old tests.

--- Tests/run2.cmd
+++ Tests/run2.cmd
@@ -1,9 +1,12 @@
 @echo off
+set NGen="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\ngen.exe"
+
 if not defined Configuration set Configuration=Debug
 if not defined NemerleBinPathRoot set NemerleBinPathRoot="%ProgramFiles%\Nemerle"
 if not defined Nemerle set Nemerle=%NemerleBinPathRoot%\Net-4.0
 set RuntimeDllPath=%~dp0\..\N2\N2.Runtime\bin\%Configuration%
 set N2CompilerDllPath=%~dp0\..\N2\N2.Compiler\bin\%Configuration%\Stage1
+rem for %%d in (%N2CompilerDllPath%\*.dll) DO %NGen% install %%d
 
 echo ---- POSITIVE TESTS ----
 set TeamCityArgs=
