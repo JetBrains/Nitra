8d8a5433d80d1a95e5168394b06db247fda18962
VladD2 <vc@rsdn.ru>
2013-02-15 21:01:17 +0400

    Add debug form.

---
+++ Grammars/Json/Tests/Sample.Json.Cs/ParseResultViewer.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Sample.Json.Cs
+{
+  partial class ParseResultViewer
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this._lbRules = new System.Windows.Forms.ListBox();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.textBox1.Location = new System.Drawing.Point(0, 0);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.textBox1.Size = new System.Drawing.Size(856, 347);
+      this.textBox1.TabIndex = 0;
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+      this.splitContainer1.Name = "splitContainer1";
+      this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // splitContainer1.Panel1
+      // 
+      this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this._lbRules);
+      this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+      this.splitContainer1.Size = new System.Drawing.Size(856, 586);
+      this.splitContainer1.SplitterDistance = 347;
+      this.splitContainer1.TabIndex = 1;
+      // 
+      // _lbRules
+      // 
+      this._lbRules.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this._lbRules.FormattingEnabled = true;
+      this._lbRules.Location = new System.Drawing.Point(12, 38);
+      this._lbRules.Name = "_lbRules";
+      this._lbRules.Size = new System.Drawing.Size(301, 186);
+      this._lbRules.TabIndex = 0;
+      // 
+      // ParseResultViewer
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(856, 586);
+      this.Controls.Add(this.splitContainer1);
+      this.Name = "ParseResultViewer";
+      this.Text = "ParseResultViewer";
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.Panel1.PerformLayout();
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.ListBox _lbRules;
+  }
+}
---
+++ Grammars/Json/Tests/Sample.Json.Cs/ParseResultViewer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sample.Json.Cs
+{
+  public partial class ParseResultViewer : Form
+  {
+    public ParseResultViewer()
+    {
+      InitializeComponent();
+    }
+
+    private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+    {
+
+    }
+  }
+}
