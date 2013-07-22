8841c28d144e12119ee83199a67c02b3c6ce0fe4
VladD2 <vc@rsdn.ru>
2013-07-22 17:44:17 +0400

    Fix sub-run.cmd

--- Tests/sub-run.cmd
+++ Tests/sub-run.cmd
@@ -1,8 +1,8 @@
-@echo off
+@echo on
 if exist %OutDir% rmdir %OutDir% /S /Q
 mkdir %OutDir%
 copy %~dp0\..\Grammars\Bin\%Configuration%\*.* %OutDir% /B /Z 1>nul
 pushd .
 cd %OutDir%
-%Nemerle%\Nemerle.Compiler.Test.exe %Tests%\*.n %Tests%\*.cs %Tests%\*.n2 -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\N2.Runtime.dll -ref:%CoreDllPath%\N2.Core.dll -macro:%N2CompilerDllPath%\N2.Compiler.dll %TeamCityArgs%
+"%Nemerle%\Nemerle.Compiler.Test.exe" %Tests%\*.n %Tests%\*.cs %Tests%\*.n2 -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\N2.Runtime.dll -ref:%CoreDllPath%\N2.Core.dll -macro:%N2CompilerDllPath%\N2.Compiler.dll %TeamCityArgs%
 popd
