09ceb8b2bd7e0772077e668c201bcc88d267f1b7
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2013-10-22 17:38:36 +0400

    Change extension  to *.nitra.

--- Tests/sub-run.cmd
+++ Tests/sub-run.cmd
@@ -4,5 +4,5 @@ mkdir %OutDir%
 copy %~dp0\..\Grammars\Bin\%Configuration%\*.* %OutDir% /B /Z 1>nul
 pushd .
 cd %OutDir%
-"%Nemerle%\Nemerle.Compiler.Test.exe" %Tests%\*.n %Tests%\*.cs %Tests%\*.n2 -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\Nitra.Runtime.dll -ref:%CoreDllPath%\Nitra.Core.dll -macro:%N2CompilerDllPath%\Nitra.Compiler.dll %TeamCityArgs%
+"%Nemerle%\Nemerle.Compiler.Test.exe" %Tests%\*.n %Tests%\*.cs %Tests%\*.nitra -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\Nitra.Runtime.dll -ref:%CoreDllPath%\Nitra.Core.dll -macro:%N2CompilerDllPath%\Nitra.Compiler.dll %TeamCityArgs%
 popd
