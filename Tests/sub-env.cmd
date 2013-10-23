d304aaf10c115b25d5259157c5aa6c588a394b7c
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2013-10-23 12:49:18 +0400

    Renaming N2 directory to Nitra

--- Tests/sub-env.cmd
+++ Tests/sub-env.cmd
@@ -3,7 +3,7 @@ set NGen="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\ngen.exe"
 if not defined Configuration set Configuration=Debug
 if not defined NemerleBinPathRoot set NemerleBinPathRoot=%ProgramFiles%\Nemerle
 if not defined Nemerle set Nemerle=%NemerleBinPathRoot%\Net-4.0
-set RuntimeDllPath=%~dp0\..\N2\Nitra.Runtime\bin\%Configuration%
-set CoreDllPath=%~dp0\..\N2\Nitra.Core\bin\%Configuration%
-set N2CompilerDllPath=%~dp0\..\N2\Nitra.Compiler\bin\%Configuration%\Stage1
+set RuntimeDllPath=%~dp0\..\Nitra\Nitra.Runtime\bin\%Configuration%
+set CoreDllPath=%~dp0\..\Nitra\Nitra.Core\bin\%Configuration%
+set N2CompilerDllPath=%~dp0\..\Nitra\Nitra.Compiler\bin\%Configuration%\Stage1
 rem for %%d in (%N2CompilerDllPath%\*.dll) DO %NGen% install %%d
