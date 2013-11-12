f46f3ded7524e5e035285ce6009daa7de4dd5d5c
VladD2 <vc@rsdn.ru>
2013-11-12 20:17:00 +0400

    Add Gherkin to Bramaglot.nproj

---
+++ Grammars/Bramaglot/Feature.cs
@@ -0,0 +1,25 @@
+﻿using Nemerle.Utility;
+
+using System.Collections.Generic;
+
+namespace DescribeBehaviour
+{
+  [Record] // macro which automatically create constructor for all fields
+  public class Feature
+  {
+    public string                Content   { get; private set; }
+    public string                AsA       { get; private set; }
+    public string                IWantTo   { get; private set; }
+    public string                SoThat    { get; private set; }
+    public IEnumerable<Scenario> Scenarios { get; private set; }
+  }
+
+  [Record] // macro which automatically create constructor for all fields
+  public class Scenario
+  {
+    public string Content { get; private set; }
+    public string Given   { get; private set; }
+    public string When    { get; private set; }
+    public string Then    { get; private set; }
+  }
+}
