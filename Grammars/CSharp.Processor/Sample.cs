ff0b3aa1880c2ce0d452039698bda8db201ddd44
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2014-02-21 18:31:00 +0400

    Simple C# conversion tool.

---
+++ Grammars/CSharp.Processor/Sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Sample
+{
+    public static class Program
+    {
+        public int Main(string[] arguments)
+        {
+            return 0;
+        }
+    }
+}
