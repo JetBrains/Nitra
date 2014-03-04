69c2ec556d1bcc034f178837ce62c743f030aeb5
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2014-03-04 18:40:40 +0400

    Some conversions for C# processor.

--- Grammars/CSharp.Processor/Sample.cs
+++ Grammars/CSharp.Processor/Sample.cs
@@ -3,11 +3,13 @@ using System.Linq;
 
 namespace Sample
 {
-    public static class Program
+  public static class Program
+  {
+    public int Main(string[] arguments)
     {
-        public int Main(string[] arguments)
-        {
-            return 0;
-        }
+      var items = new int[] { 1, 2, 3, 4, 5, 6, 7 }.Where(x => x % 2 == 0);
+      foreach (var x in items) Console.WriteLine(x);
+      return 0;
     }
+  }
 }
