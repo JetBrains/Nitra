2060da3f00ff3e2a3c5cebd21a6d5a320011af52
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2014-03-12 21:43:52 +0400

    Fix conversion.

--- Grammars/CSharp.Processor/Sample.cs
+++ Grammars/CSharp.Processor/Sample.cs
@@ -10,6 +10,9 @@ namespace Sample
       object x;
       if (!false)
         x = new int[] { 1, 2, 3, 4, 5, 6, 7, 0 }.Where(x => x > 4);
+
+      new int[] { 1, 2, 3, 4, 5, 6, 7, 0 }.Where(x => x < 6);
+
       var items = new int[] { 1, 2, 3, 4, 5, 6, 7, 0 }.Where(x => x % 2 == 0);
       foreach (var x in items) Console.WriteLine(x);
       return 0;
