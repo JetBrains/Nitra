0bc7dc42a8b3a3c29d1fce32e96cdd284af28f66
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2014-04-17 17:59:50 +0400

    Add quasi-quotation test.

--- Tests/sub-run.cmd
+++ Tests/sub-run.cmd
@@ -4,5 +4,5 @@ mkdir %OutDir%
 copy %~dp0\..\Grammars\Bin\%Configuration%\*.* %OutDir% /B /Z 1>nul
 pushd .
 cd %OutDir%
-"%Nemerle%\Nemerle.Compiler.Test.exe" %Tests%\*.n %Tests%\*.cs %Tests%\*.nitra -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\Nitra.Runtime.dll -ref:%CoreDllPath%\Nitra.Core.dll -macro:%CompilerDllPath%\Nitra.Compiler.dll %TeamCityArgs%
+"%Nemerle%\Nemerle.Compiler.Test.exe" %Tests%\*.n %Tests%\*.cs %Tests%\*.nitra -output:%OutDir% -ref:System.Core -ref:%RuntimeDllPath%\Nitra.Runtime.dll -ref:%CoreDllPath%\Nitra.Core.dll -macro:%CompilerDllPath%\Nitra.Compiler.dll -macro:%QuoteDllPath%\Nitra.Quote.dll %TeamCityArgs%
 popd
