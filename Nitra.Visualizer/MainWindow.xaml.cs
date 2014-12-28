4b2c17b0ef9e949dbf584b0078fb64d8fc399587
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2014-12-28 15:24:41 +0300

    Rename IDeclarableParseTree to IMappedParseTree.

--- Nitra.Visualizer/MainWindow.xaml.cs
+++ Nitra.Visualizer/MainWindow.xaml.cs
@@ -425,7 +425,7 @@ namespace Nitra.Visualizer
         _parseTree = _parseResult.CreateParseTree();
 
 // ReSharper disable once SuspiciousTypeConversion.Global
-      var root = _parseTree as IDeclarableParseTree<IAst, IAst>;
+      var root = _parseTree as IMappedParseTree<IAst, IAst>;
       if (root != null)
       {
         var declarationRoot = DeclarationRoot<IAst>.Create(null, root);
