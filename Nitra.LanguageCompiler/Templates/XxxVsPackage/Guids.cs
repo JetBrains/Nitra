f1fd3c4af76a19ebdc2c382e957c45cfef13d7b6
VladD2 <vc@rsdn.ru>
2015-01-30 22:41:42 +0300

    Work on Nitra.LanguageCompiler. Guigs generation references.

--- Nitra.LanguageCompiler/Templates/XxxVsPackage/Guids.cs
+++ Nitra.LanguageCompiler/Templates/XxxVsPackage/Guids.cs
@@ -8,6 +8,7 @@ namespace XCompanyX.XxxVsPackage
     {
         public const string GuidXxxVsPackagePkgString    = "85c0a77e-a20e-4f36-a1e4-1ec513b41b0e";
         public const string GuidXxxVsPackageCmdSetString = "1e508b08-3bef-4197-81c1-06b84a49f118";
+        public const string GuidProject                  = "09CD39E9-5139-48B5-A1AE-B8EB59CEE1CD";
 
         public static readonly Guid GuidXxxVsPackageCmdSet = new Guid(GuidXxxVsPackageCmdSetString);
     };
