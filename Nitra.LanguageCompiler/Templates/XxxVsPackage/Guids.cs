c78c13d77da97addd58b26974c0a12135f1c76a4
VladD2 <vc@rsdn.ru>
2015-01-30 22:46:39 +0300

    Work on Nitra.LanguageCompiler. Change guids case.

--- Nitra.LanguageCompiler/Templates/XxxVsPackage/Guids.cs
+++ Nitra.LanguageCompiler/Templates/XxxVsPackage/Guids.cs
@@ -6,8 +6,8 @@ namespace XCompanyX.XxxVsPackage
 {
     static class GuidList
     {
-        public const string GuidXxxVsPackagePkgString    = "85c0a77e-a20e-4f36-a1e4-1ec513b41b0e";
-        public const string GuidXxxVsPackageCmdSetString = "1e508b08-3bef-4197-81c1-06b84a49f118";
+        public const string GuidXxxVsPackagePkgString    = "85C0A77E-A20E-4F36-A1E4-1EC513B41B0E";
+        public const string GuidXxxVsPackageCmdSetString = "1E508B08-3BEF-4197-81C1-06B84A49F118";
         public const string GuidProject                  = "09CD39E9-5139-48B5-A1AE-B8EB59CEE1CD";
 
         public static readonly Guid GuidXxxVsPackageCmdSet = new Guid(GuidXxxVsPackageCmdSetString);
