4b41fec6b947d49ed0e25d9b25b374ef7f4b697b
VladD2 <vc@rsdn.ru>
2015-02-11 17:49:00 +0300

    Work on Nitra.LanguageCompiler. Add support of brackets navigation.

---
+++ Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/KeyBinding/KeyBindingFilterProvider.cs
@@ -0,0 +1,34 @@
+﻿using Nitra.VisualStudio;
+using Nitra.VisualStudio.KeyBinding;
+
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+using VSConstants = Microsoft.VisualStudio.VSConstants;
+
+namespace XXNamespaceXX
+{
+  [Export(typeof(IVsTextViewCreationListener))]
+  [ContentType("XXLanguageXX")]
+  [TextViewRole(PredefinedTextViewRoles.Editable)]
+  internal class KeyBindingCommandFilterProvider : IVsTextViewCreationListener
+  {
+    [Import]
+    private ITextDocumentFactoryService _textDocumentFactoryService = null;
+
+    public void VsTextViewCreated(IVsTextView textViewAdapter)
+    {
+      new KeyBindingCommandFilter(textViewAdapter, _textDocumentFactoryService);
+    }
+  }
+}
