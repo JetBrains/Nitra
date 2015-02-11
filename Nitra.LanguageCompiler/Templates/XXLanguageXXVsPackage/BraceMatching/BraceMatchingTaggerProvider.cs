9836b1a22c1bdfa520401727484024870a04840a
VladD2 <vc@rsdn.ru>
2015-02-11 18:30:22 +0300

    Work on Nitra.LanguageCompiler. Add support of brace matching.

---
+++ Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/BraceMatching/BraceMatchingTaggerProvider.cs
@@ -0,0 +1,37 @@
+﻿using Nitra.VisualStudio;
+using Nitra.VisualStudio.Parsing;
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Adornments;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace XXNamespaceXX
+{
+  [Export(typeof(IViewTaggerProvider) )]
+  [ContentType("XXLanguageXX")]
+  [TagType(typeof(TextMarkerTag))]
+  internal sealed class BraceMatchingTaggerProvider : IViewTaggerProvider
+  {
+    [Import]
+    private ITextDocumentFactoryService _textDocumentFactoryService = null;
+
+    public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+    {
+      BraceMatchingTagger braceMatchingTagger;
+      if (buffer.Properties.TryGetProperty(TextBufferProperties.BraceMatchingTagger, out braceMatchingTagger))
+        return (ITagger<T>)braceMatchingTagger;
+
+      var parseAgent = Utils.TryGetOrCreateParseAgent(buffer, _textDocumentFactoryService, NitraPackage.Instance.Language);
+      var tagger = new BraceMatchingTagger(Constants.ErrorClassificationTypeName, parseAgent, textView, buffer);
+      buffer.Properties.AddProperty(TextBufferProperties.BraceMatchingTagger, tagger);
+      return (ITagger<T>)tagger;
+    }
+  }
+}
---
+++ Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/Highlighting/ErrorClassificationDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace XXNamespaceXX
+{
+  [Export(typeof(EditorFormatDefinition))]
+  [ClassificationType(ClassificationTypeNames = Constants.ErrorClassificationTypeName)]
+  [Name(Constants.ErrorClassificationTypeName)]
+  [UserVisible(true)]
+  [Order(Before = Priority.Default)]
+  public sealed class ErrorClassificationDefinition : ClassificationFormatDefinition
+  {
+    public ErrorClassificationDefinition()
+    {
+      this.DisplayName = Constants.ErrorClassificationTypeName;
+      this.BackgroundColor = Colors.Red;
+      this.ForegroundColor = Colors.White;
+    }
+  }
+}
