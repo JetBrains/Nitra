492936bf477b075cef2433a9da25803ec8501939
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2015-03-17 18:09:54 +0300

    Remove old Visual Studio Extension references from installer.

--- Installer/ScriptActions.js
+++
@@ -1,7 +0,0 @@
-function UpdateFileTimestamp() {
-  var filePath = Session.Property("CustomActionData");
-  var fso = new ActiveXObject("Scripting.FileSystemObject");
-  var fileStream = fso.OpenTextFile(filePath, /*ForWrite*/2,  /*CreateNew*/true, /*TristateFalse*/0);
-  fileStream.Write("It's never too late to have a happy childhood.");
-  fileStream.Close();
-}
