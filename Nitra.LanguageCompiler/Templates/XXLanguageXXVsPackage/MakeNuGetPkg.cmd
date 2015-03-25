dd3f2fc12ae993bba4c6665f65bc11c5bd05dcb9
VladD2 <vc@rsdn.ru>
2015-03-25 18:48:30 +0300

    Work on Nitra.LanguageCompiler

---
+++ Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/MakeNuGetPkg.cmd
@@ -0,0 +1,14 @@
+echo Star of %0
+set Configuration=%1
+set ExternalTools=%2
+set Output=%3
+set version=%4
+IF NOT "%version%" == "" set VersionAttr=;VersionAttr=%version%
+set ProjectDir=%~dp0
+set NuGet=%ExternalTools%\NuGet.exe
+set Properties=Configuration=%Configuration%;Platform=x86%VersionAttr%
+
+echo Properties=%Properties%
+
+%NuGet% pack %ProjectDir%\XXLanguageXXVsPackage.csproj -OutputDirectory %Output% -Properties "%Properties%" 
+echo End of %0
--- NuGet/MakeNuGetPkgs.cmd
+++ NuGet/MakeNuGetPkgs.cmd
@@ -1,3 +1,4 @@
+echo Star off %0
 set version=%1
 set Configuration=Debug
 set Solution=..
@@ -6,10 +7,9 @@ set NuGet=%ExternalTools%\NuGet.exe
 set Output=%Solution%\bin\%Configuration%\NuGetSource
 set Properties=Configuration=%Configuration%;Solution=%Solution%;version=%version%
 
-echo Star %0
-echo version='%version%'
 mkdir %Output%
 %NuGet% pack %Solution%\Nitra\Nitra.Core\Nitra.Core.nproj -OutputDirectory %Output% -Properties "%Properties%" -IncludeReferencedProjects
 %NuGet% pack %Solution%\NuGet\Nitra.Compiler.nuspec       -OutputDirectory %Output% -Properties "%Properties%"
 %NuGet% pack %Solution%\NuGet\Nitra.VisualStudio.nuspec   -OutputDirectory %Output% -Properties "%Properties%"
 %NuGet% pack %Solution%\NuGet\Nitra.Tools.nuspec          -OutputDirectory %Output% -Properties "%Properties%" -NoPackageAnalysis
+echo End off %0
