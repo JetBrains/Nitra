3a11e4c419d5f95052d5f042c5c4c78c6e0fd3f9
VladD2 <vc@rsdn.ru>
2015-04-02 00:44:31 +0300

    Work on ReSharper plagin. Don't merge with this commit!

---
+++ Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/ReSharper/SolutionStart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ProjectModel;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Data.Core;
+using JetBrains.VsIntegration.Shell;
+
+namespace Nitra
+{
+  [SolutionComponent]
+  public class SolutionStart
+  {
+    public SolutionStart(RawVsServiceProvider rawVsServiceProvider)
+    {
+      
+      //var xx = ServiceProvider.GlobalProvider;
+      
+      Trace.Assert(false);
+      var xx = (IVsDataHostService)rawVsServiceProvider.Value.GetService<IVsDataHostService, IVsDataHostService>();
+      //Trace.Assert(serviceProvider != null);
+      //NitraPackage.Init(serviceProvider);
+
+      xx = xx;
+    }
+  }
+
+  [ZoneMarker]
+  public class ZoneMarker
+  {
+  }
+}
