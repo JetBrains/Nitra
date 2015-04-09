623e33cc4e809ac5dbfba473448facec57dbe622
VladD2 <vc@rsdn.ru>
2015-04-09 23:01:55 +0300

    Work on ReSharper plagin. Add FileType.

--- Nitra.LanguageCompiler/Templates/FileAndContentTypeDefinitions/NitraContentTypeDefinition.cs
+++ Nitra.LanguageCompiler/Templates/FileAndContentTypeDefinitions/NitraContentTypeDefinition.cs
@@ -16,3 +16,62 @@ namespace XXNamespaceXX
     internal static string[] FileExtensions = { "XXFileExtensionsXX" };
   }
 }
+
+namespace XXNamespaceXX
+{
+  using JetBrains.ProjectModel;
+  using JetBrains.ReSharper.Psi;
+  using JetBrains.ReSharper.Psi.Parsing;
+  using JetBrains.Text;
+  using JetBrains.UI.Icons;
+  
+  [ProjectFileTypeDefinition(Name)]
+  public class XXLanguageXXFileType : KnownProjectFileType
+  {
+    public new const string Name = "XXLanguageXX";
+    public new static readonly XXLanguageXXFileType Instance = new XXLanguageXXFileType();
+
+    private XXLanguageXXFileType()
+      : base(Name, "XXLanguageXX", NitraFileExtensionsAndContentTypeDefinition.FileExtensions)
+    {
+    }
+
+    protected XXLanguageXXFileType(string name)
+      : base(name)
+    {
+    }
+
+    protected XXLanguageXXFileType(string name, string presentableName)
+      : base(name, presentableName)
+    {
+    }
+  }
+
+  [ProjectFileType(typeof(XXLanguageXXFileType))]
+  public class XXLanguageXXProjectFileLanguageService : ProjectFileLanguageService
+  {
+    public XXLanguageXXProjectFileLanguageService(XXLanguageXXFileType projectFileType)
+      : base(projectFileType)
+    {
+    }
+
+    protected override PsiLanguageType PsiLanguageType
+    {
+      get { return null/*XXLanguageXXLanguage.Instance*/; }
+    }
+
+    public override IconId Icon
+    {
+      get
+      {
+        //return LexPluginSymbolThemedIcons.PsiFile.Id;
+        return null;
+      }
+    }
+
+    public override ILexerFactory GetMixedLexerFactory(ISolution solution, IBuffer buffer, IPsiSourceFile sourceFile = null)
+    {
+      return null;
+    }
+  }
+}
