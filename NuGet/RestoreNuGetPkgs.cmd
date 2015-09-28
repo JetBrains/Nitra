40fc9ad107acf0993cfc29e83265afe362fbd6ac
Stanislav Matveev <Stanislav.Matveev@jetbrains.com>
2015-09-28 19:42:16 +0300

    Working on build sequence.

--- NuGet/RestoreNuGetPkgs.cmd
+++ NuGet/RestoreNuGetPkgs.cmd
@@ -1,8 +1,8 @@
 set SolutionDir=%~dp0..\
 set ProjectDir=%1
-set NuGetLocalSource=%2
+set NuGetSource=%2
 set ExternalTools=%SolutionDir%\ExternalTools
 set NuGet=%ExternalTools%\NuGet.exe
 
-%NuGet% update  %ProjectDir%\packages.config -Source "%NuGetLocalSource%"
-%NuGet% restore %ProjectDir%\packages.config -PackagesDirectory %ProjectDir%\packages -Source "%NuGetLocalSource%;https://www.nuget.org/api/v2"
+%NuGet% update  %ProjectDir%\packages.config -Source %NuGetSource%
+%NuGet% restore %ProjectDir%\packages.config -PackagesDirectory %ProjectDir%\packages -Source %NuGetSource%
