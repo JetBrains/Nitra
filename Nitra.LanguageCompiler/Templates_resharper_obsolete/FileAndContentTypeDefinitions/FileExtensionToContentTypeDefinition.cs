b343017fe549efc26aa04a162054ccf1ddbd4a33
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-02-18 10:37:18 +0300

    backup copy of Nitra.LanguageCompiler/Templates

---
+++ Nitra.LanguageCompiler/Templates_resharper_obsolete/FileAndContentTypeDefinitions/FileExtensionToContentTypeDefinition.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+
+namespace XXNamespaceXX
+{
+  internal static partial class NitraFileExtensionsAndContentTypeDefinition
+  {
+    [Export]
+    [FileExtension("XXFileExtensionXX")]
+    [ContentType("XXLanguageXX")]
+    internal static FileExtensionToContentTypeDefinition XXFileExtensionNameXXFileExtensionDefinition = null;
+  }
+}
---
+++ Nitra.LanguageCompiler/Templates_resharper_obsolete/XXLanguageXXVsPackage/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XXNamespaceXX
+{
+  public class Constants
+  {
+    public const string ErrorClassificationTypeName = "XXLanguageXXError";
+  }
+}
