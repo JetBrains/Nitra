fff599d293e0145af3134a62f4411a7aa6ae1e1f
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-02-18 13:01:52 +0300

    removing all R# stuff from Template

--- Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/Guids.cs
+++ Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/Guids.cs
@@ -8,7 +8,7 @@ namespace XXNamespaceXX
     {
         public const string GuidXXLanguageXXVsPackagePkgString    = "85C0A77E-A20E-4F36-A1E4-1EC513B41B0E";
         public const string GuidXXLanguageXXVsPackageCmdSetString = "1E508B08-3BEF-4197-81C1-06B84A49F118";
-        public const string GuidProject                  = "09CD39E9-5139-48B5-A1AE-B8EB59CEE1CD";
+        public const string GuidProject                           = "09CD39E9-5139-48B5-A1AE-B8EB59CEE1CD";
 
         public static readonly Guid GuidXXLanguageXXVsPackageCmdSet = new Guid(GuidXXLanguageXXVsPackageCmdSetString);
     };
