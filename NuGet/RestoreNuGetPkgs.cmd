28d888351d3c1cb4cb9e16079557351533fa3001
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-02-19 14:54:36 +0300

    Nitra.VisualStudio.Plugin compiles VSIX, but does not installs yet

--- NuGet/RestoreNuGetPkgs.cmd
+++ NuGet/RestoreNuGetPkgs.cmd
@@ -4,5 +4,4 @@ set NuGetSource=%2
 set ExternalTools=%SolutionDir%\ExternalTools
 set NuGet=%ExternalTools%\NuGet.exe
 
-%NuGet% update  %ProjectDir%\packages.config -Source %NuGetSource%
 %NuGet% restore %ProjectDir%\packages.config -PackagesDirectory %ProjectDir%\packages -Source %NuGetSource%
