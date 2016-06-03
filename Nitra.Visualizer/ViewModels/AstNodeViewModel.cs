447b0bdde853ed83e18e7c1423d7ed3467e733ab
VladD2 <vc@rsdn.ru>
2016-06-04 00:38:22 +0300

    [ClientServer] Work on AstReflection.

---
+++ Nitra.Visualizer/ViewModels/AstNodeViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Nitra.ClientServer.Client;
+using Nitra.ClientServer.Messages;
+
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitra.Visualizer.ViewModels
+{
+  public class AstNodeViewModel : ReactiveObject
+  {
+    readonly ObjectDescriptor _objectDescriptor;
+    readonly NitraClient _client;
+
+    [Reactive]
+    public bool                           NeedLoadContent { get; private set; }
+    public ReactiveList<AstNodeViewModel> Items           { get; set; }
+
+    public AstNodeViewModel(NitraClient client, ObjectDescriptor objectDescriptor)
+    {
+      _client           = client;
+      _objectDescriptor = objectDescriptor;
+      Items = new ReactiveList<AstNodeViewModel>();
+      if (objectDescriptor.IsObject || objectDescriptor.IsSeq && objectDescriptor.Count > 0)
+      {
+        NeedLoadContent = true;
+        Items.Add(null);
+      }
+    }
+  }
+}
