025caf0b604e152ef2786ec82d94805a56a0e3ae
VladD2 <vc@rsdn.ru>
2016-09-12 17:35:19 +0400

    Work on VS plagin generator.

--- Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/Guids.cs
+++ Nitra.LanguageCompiler/Templates/XXLanguageXXVsPackage/Guids.cs
@@ -10,8 +10,6 @@ namespace XXNamespaceXX
         public const string PackageGuid                           = "7751d8fd-73c8-40bd-be2b-7aa6f7a74faa";
         public const string GuidXXLanguageXXVsPackageCmdSetString = "1E508B08-3BEF-4197-81C1-06B84A49F118";
         public const string GuidProject                           = "09CD39E9-5139-48B5-A1AE-B8EB59CEE1CD";
-        /// <summary>The default namespace this project compiles with, so that manifest resource names can be calculated for embedded resources.</summary>
-        public const string DefaultNamespace = "XXNamespaceXX";
 
         public static readonly Guid GuidXXLanguageXXVsPackageCmdSet = new Guid(GuidXXLanguageXXVsPackageCmdSetString);
     };
