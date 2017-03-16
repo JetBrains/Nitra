c0e28bc2344b72c761579e4501f1161eb260fde2
VladD2 <vc@rsdn.ru>
2017-03-16 03:04:45 +0300

    Move to VS2017

--- Ide/NitraCommonVSIX/NavigateTo/NitraNavigateToItemDisplay.cs
+++ Ide/NitraCommonVSIX/NavigateTo/NitraNavigateToItemDisplay.cs
@@ -7,9 +7,40 @@ using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Shell;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.Language.Intellisense;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Imaging;
 
 namespace Nitra.VisualStudio.NavigateTo
 {
+  internal class NavigateToSymbolFilters
+  {
+    [Name("Navigate To Nitra Symbol"), Order(After = "Navigate To Class"), Export(typeof(FilterShortcutDefinition))]
+    public sealed class NitraSymbolFilterShortcut : FilterShortcutDefinition
+    {
+      public NitraSymbolFilterShortcut()
+      {
+        base.ActivationSequence = "n";
+        base.IsDelimiterRequired = true;
+        base.Description = "desc";
+        base.Button = new ButtonDefinition(KnownMonikers.IntellisenseKeyword, "desc", null);
+      }
+    }
+
+    [Filter("Navigate To All Nitra Symbol"), FilterShortcut("Navigate To Nitra Symbol"), Export]
+    internal FilterToShortcutDefinition nitraSymbolFilterMapping;
+  }
+
+  [Name("Navigate To Nitra Symbol"), Order(After = "Navigate To Class"), Export(typeof(FilterDefinition))]
+  internal sealed class NavigateToNitraSymbolFilter : KindFilterDefinition
+  {
+    public NavigateToNitraSymbolFilter()
+    {
+      base.Kind = "NitraSymbol";
+      //base.Button = new ButtonDefinition(KnownMonikers.IntellisenseKeyword, "desc", null);
+    }
+  }
+
   class NitraNavigateToItemDisplay : INavigateToItemDisplay, INavigateToItemDisplay2
   {
     private IGlyphService _glyphService;
--- Ide/NitraCommonVSIX/NavigateTo/NitraNavigateToItemProvider.cs
+++ Ide/NitraCommonVSIX/NavigateTo/NitraNavigateToItemProvider.cs
@@ -20,11 +20,11 @@ namespace Nitra.VisualStudio.NavigateTo
     public void StartSearch(INavigateToCallback callback, string searchValue)
     {
       var factory = new NitraNavigateToItemDisplayFactory(_serviceProvider, _glyphService);
-      callback.AddItem(new NavigateToItem("RulePrefix",  "rule", "Nitra", "aaa", null, MatchKind.Prefix, factory));
-      callback.AddItem(new NavigateToItem("ExactRule", "rule", "Nitra", "aaa", null, MatchKind.Exact, factory));
-      callback.AddItem(new NavigateToItem("RuleRegular", "rule", "Nitra", "aaa", null, MatchKind.Regular, factory));
-      callback.AddItem(new NavigateToItem("SubstringRule", "rule", "Nitra", "aaa", null, MatchKind.Substring, factory));
-      callback.AddItem(new NavigateToItem("None", "rule", "Nitra", "aaa", null, MatchKind.None, factory));
+      callback.AddItem(new NavigateToItem("RulePrefix",   "OtherSymbol",  "Nitra", "aaa", null, MatchKind.Prefix, factory));
+      callback.AddItem(new NavigateToItem("ExactRule",     "OtherSymbol", "Nitra", "aaa", null, MatchKind.Exact, factory));
+      callback.AddItem(new NavigateToItem("RuleRegular",   "OtherSymbol", "Nitra", "aaa", null, MatchKind.Regular, factory));
+      callback.AddItem(new NavigateToItem("SubstringRule", "NitraSymbol", "Nitra", "aaa", null, MatchKind.Substring, factory));
+      callback.AddItem(new NavigateToItem("None",          "NitraSymbol", "Nitra", "aaa", null, MatchKind.None, factory));
       //callback.Done();
     }
 
